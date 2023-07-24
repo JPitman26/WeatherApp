719c772393c9e3820b67753783ea16de63b9c111
JackPitman <80924402+JPitman26@users.noreply.github.com>
2023-07-24 18:40:12 +0100

    Update script.js
    This gets the value of the input field with the id "cityName".

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function getWeather() {
     // This function gets the weather forecast for a city name.
   
+    var cityName = document.getElementById("cityName").value;
+    // This gets the value of the input field with the id "cityName".
+  
