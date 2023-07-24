5319bac74d0a88cd5e725848a9d3a6eea554e605
JackPitman <80924402+JPitman26@users.noreply.github.com>
2023-07-24 18:40:36 +0100

    Update script.js
    This creates a URL that can be used to get the weather forecast for the city name

--- script.js
+++ script.js
@@ -9,3 +9,7 @@ function getWeather() {
       document.getElementById("weather").innerHTML = "Please enter a city name.";
       return;
     }
+  
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=665a35ed47396b1878a1a3b9ba8fa8ea";
+    // This creates a URL that can be used to get the weather forecast for the city name.
+  
